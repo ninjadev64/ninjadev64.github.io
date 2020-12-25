22497ece96289abea79b9a84e395d61b34def295
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2020-12-25 20:32:17 +0000

    Update sketch.js

--- Monkey Go Happy/sketch.js
+++ Monkey Go Happy/sketch.js
@@ -13,7 +13,7 @@ var END = 0;
 var gameState = PLAY;
 
 function preload(){
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
+  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
   
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png"); 
@@ -58,7 +58,6 @@ function spawnObstacle() {
   obstacle.scale = 0.1;
   obstacle.velocityX = -5;
   obstacle.setCollider("circle");
-  obstacle.debug = true;
   obstacleGroup.add(obstacle);
 }
 
